672dad3e838a8d7b60e4c6492d3f7605d79f99e3
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 08:03:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,7 @@ def countdown(integer)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(integer)
   until integer == 0
     sleep(1)
     puts "#{integer} SECOND(S)!"
